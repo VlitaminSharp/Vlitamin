8564e7a98ae9eccacd7b4ecc0feb5b6f706219eb
kywooo <rlaalsgud452@daum.net>
2015-12-01 17:45:25 +0900

    add

--- VitaminSharp/Champions/Tristana.cs
+++ VitaminSharp/Champions/Tristana.cs
@@ -29,7 +29,7 @@ namespace VitaminSharp.Champions
 
         public override void OnMenu()
         {
-            var movingMenu = menu.AddSubMenu(new Menu("OrbWalker", "orbWalker"));
+            var movingMenu = new Menu("OrbWalker", "orbWalker");
             moving = new Orbwalking.Orbwalker(movingMenu);
             menu.AddSubMenu(movingMenu);
 
